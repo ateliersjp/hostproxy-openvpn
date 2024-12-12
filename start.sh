7922c62fb5578de86ee02e8799a6439c72c34a95
ateliersjp <146078826+ateliersjp@users.noreply.github.com>
2024-12-12 15:34:19 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ if [ "$1" = 'hostproxy' ]; then
   shift
 fi
 
-openvpn /dev/stdin | awk '{ print } /Initialization Sequence Completed/ { system("pkill sleep") }' &
+openvpn /dev/stdin | tee >(awk '/Initialization Sequence Completed/ {
+  exit 1
+}' || pkill sleep) &
 sleep 60 && exit
 hostproxy "$@" &
 exec curl -s "https://ppng.io/$(hexdump -ve '1/1 "%02x"' -n 32 /dev/random)"
